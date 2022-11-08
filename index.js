89959f17ac109b4c682f7016b6887361ac132fa7
unknown <marufmozumder7@gmail.com>
2022-11-08 11:21:43 +0100

    setup-done

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const port = process.env.PORT|| 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Its happening');
+})
+
+app.listen(port, () => {
+    console.log('running');
+})
