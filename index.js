8cd820b9cc70647297ce5eb5230e0c0bd2bad416
unknown <marufmozumder7@gmail.com>
2022-11-08 11:45:10 +0100

    services-added-in-mongodb

--- index.js
+++ index.js
@@ -9,17 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 //connection with mongodb
-console.log(process.env.DB_USER);
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.19qwu6y.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 
 app.get('/', (req, res) => {
