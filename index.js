e6874b1c0bbd32f89f1dceae297412055368f68e
unknown <marufmozumder7@gmail.com>
2022-11-08 11:51:06 +0100

    services-added-in-localhost

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.19qwu6y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const main = async() => {
+try{
+const database = client.db("photography").collection("services");
+
+app.get('/services', async(req,res) => {
+    const query = {};
+    const cursor = database.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+})
+}
+catch(error){
+console.log(error.name, error.message, error.stack);
+}
+}
+
+main().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
